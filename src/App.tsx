c8d2fce1cd1daa257cfc7c406817515e63161cc9
Vincent Tellier <vincent.lt.tellier@gmail.com>
2020-12-08 15:36:26 +0000

    First draft of the design

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,48 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+import AppBar from '@material-ui/core/AppBar';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import { Typography } from '@material-ui/core';
+import {
+    List,
+    ListItem
+} from '@material-ui/core';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Corvée-course</h1>
-      </header>
+    <div>
+      <AppBar position="static">
+        <h1>Corvée-courses</h1>
+      </AppBar>
+      <Grid container spacing={3}>
+        <Grid item xs>
+          <Paper>
+            <Typography gutterBottom variant="h4" component="h2">Menus</Typography>
+            <List>
+              <ListItem button>Soupe aux choux</ListItem>
+              <ListItem button>Cassoulet</ListItem>
+            </List>
+            <Button>Ajouter un plat</Button>
+          </Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper>
+            Les à-côté
+          </Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper>
+            Ingrédients
+          </Paper>
+        </Grid>
+        <Grid item xs>
+          <Paper>
+            Apercu de la liste
+          </Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 }
