7e6a435cf29ba683da488b97000cf7bc0dd88c60
Vincent Tellier <vincent.lt.tellier@gmail.com>
2020-12-10 15:50:50 +0000

    Made the stepper navigation working

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
 import Paper from '@material-ui/core/Paper';
 import Container from '@material-ui/core/Container';
 import { Typography } from '@material-ui/core';
 import {
     Stepper,
     Step,
+    StepLabel,
     StepButton,
     Button
 } from '@material-ui/core';
@@ -28,37 +30,76 @@ const useStyles = makeStyles((theme) => ({
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
+  container: {
+    paddingBottom: 150,
+  },
+  appBar: {
+    top: 'auto',
+    bottom: 0,
+  },
 }));
 
+function getSteps() {
+    return [
+        'Définition des repas',
+        'Les à-côtés',
+        'Récapitulatif'
+    ];
+}
+
+function getStepContent(step: number, classes: any) {
+  switch (step) {
+    case 0:
+      return (<Meals classes={classes} />);
+    case 1:
+      return (<Sidelines classes={classes} />);
+    case 2:
+      return (
+        <Paper className={classes.paper}>
+          <Typography gutterBottom variant="h4" component="h2"> Apercu </Typography>
+        </Paper>
+      );
+    default:
+      return 'Unknown step';
+  }
+}
+
 function App() {
-  const classes = useStyles();
-  return (
-    <div>
-      <AppBar position="static">
-        <h1>Corvée-courses</h1>
-      </AppBar>
-      <Container maxWidth="md">
-          {true ? (<Meals classes={classes} />) : (
-              true ? (<Sidelines classes={classes} />) : (
-                <Paper className={classes.paper}>
-                  <Typography gutterBottom variant="h4" component="h2"> Apercu </Typography>
-                </Paper>
-              )
-          ) }
-        <Stepper>
-          <Step>
-            <span>Définition des repas</span>
-          </Step>
-          <Step>
-            <span>Les à-côtés</span>
-          </Step>
-          <Step>
-            <span>Récapitulatif</span>
-          </Step>
-        </Stepper>
-      </Container>
-    </div>
-  );
+    const classes = useStyles();
+    const steps = getSteps();
+    const [activeStep, setActiveStep] = React.useState(0);
+    const [completed, setCompleted] = React.useState<{ [k: number]: boolean }>({});
+  
+    const handleStep = (step: number) => () => {
+        console.log(step);
+        setActiveStep(step);
+    };
+  
+    return (
+      <div>
+        <AppBar position="static">
+          <h1>Corvée-courses</h1>
+        </AppBar>
+        <Container maxWidth="md" className={classes.container}>
+  
+            { getStepContent(activeStep, classes) }
+  
+        </Container>
+        <AppBar position="fixed" className={classes.appBar}>
+            <Stepper activeStep={activeStep} alternativeLabel nonLinear>
+              {
+                  steps.map((label, index) => (
+                      <Step key={label}>
+                          <StepButton onClick={handleStep(index)} completed={completed[index]}>
+                              {label}
+                          </StepButton>
+                      </Step>
+                  ))
+              }
+            </Stepper>
+        </AppBar>
+      </div>
+    );
 }
 
 export default App;
