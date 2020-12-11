651909741333d804bac4ddd9c740cd19a715e2e2
Vincent Tellier <vincent.lt.tellier@gmail.com>
2020-12-11 13:53:52 +0000

    Turned App from a function to a class component

--- src/App.tsx
+++ src/App.tsx
@@ -43,78 +43,86 @@ const styles = (theme: Theme) => createStyles({
   },
 });
 
-function getSteps() {
-    return [
-        'Définition des repas',
-        'Les à-côtés',
-        'Récapitulatif'
-    ];
-}
-
-function getStepContent(step: number, classes: any) {
-  switch (step) {
-    case 0:
-      return (<Meals />);
-    case 1:
-      return (<Sidelines />);
-    case 2:
-      return (
-        <div>
-          <Typography gutterBottom variant="h4" component="h2"> Voici la liste des courses </Typography>
-          <Typography gutterBottom variant="h5" component="h3"> Ajustez la en fonction de ce que vous avez déjà dans vos équipés </Typography>
-        </div>
-      );
-    default:
-      return 'Unknown step';
-  }
-}
-
 type AppProps = {
-    classes: any
-}
+    classes: any,
+};
 
-function App(props: AppProps) {
-    const { classes } = props;
-    const steps = getSteps();
-    const [activeStep, setActiveStep] = React.useState(0);
-    const [completed, setCompleted] = React.useState<{ [k: number]: boolean }>({});
-  
-    const handleStep = (step: number) => () => {
-        console.log(step);
-        setActiveStep(step);
-    };
-  
-    return (
-      <div>
-        <AppBar position="static">
-          <Toolbar>
-            <Typography variant="h6" className={classes.title}>
-              Ravitaillement
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <Container maxWidth="md" className={classes.container}>
-  
-          <Paper elevation={1} className={classes.paper}>
-            { getStepContent(activeStep, classes) }
-          </Paper>
-  
-        </Container>
-        <AppBar position="fixed" className={classes.appBar}>
-            <Stepper activeStep={activeStep} alternativeLabel nonLinear>
-              {
-                  steps.map((label, index) => (
-                      <Step key={label}>
-                          <StepButton onClick={handleStep(index)} completed={completed[index]}>
-                              {label}
-                          </StepButton>
-                      </Step>
-                  ))
-              }
-            </Stepper>
-        </AppBar>
-      </div>
-    );
+type AppState = {
+    activeStep: number,
+};
+
+class App extends React.Component<AppProps,AppState> {
+    constructor(props:AppProps) {
+        super(props);
+        this.state = {
+            activeStep: 0,
+        };
+    }
+    getSteps() {
+        return [
+            'Définition des repas',
+            'Les à-côtés',
+            'Récapitulatif'
+        ];
+    }
+    getStepContent(step: number, classes: any) {
+        switch (step) {
+            case 0:
+            return (<Meals />);
+            case 1:
+            return (<Sidelines />);
+            case 2:
+            return (
+                <div>
+                <Typography gutterBottom variant="h4" component="h2"> Voici la liste des courses </Typography>
+                <Typography gutterBottom variant="h5" component="h3"> Ajustez la en fonction de ce que vous avez déjà dans vos équipés </Typography>
+                </div>
+            );
+            default:
+            return 'Unknown step';
+        }
+    }
+    render() {
+        const { classes } = this.props;
+        const steps = this.getSteps();
+    
+        const handleStep = (step: number) => () => {
+            console.log(step);
+            this.setState({ activeStep:step });
+        };
+    
+        return (
+        <div>
+            <AppBar position="static">
+            <Toolbar>
+                <Typography variant="h6" className={classes.title}>
+                    Ravitaillement
+                </Typography>
+            </Toolbar>
+            </AppBar>
+            <Container maxWidth="md" className={classes.container}>
+    
+            <Paper elevation={1} className={classes.paper}>
+                { this.getStepContent(this.state.activeStep, classes) }
+            </Paper>
+    
+            </Container>
+            <AppBar position="fixed" className={classes.appBar}>
+                <Stepper activeStep={this.state.activeStep} alternativeLabel nonLinear>
+                {
+                    steps.map((label, index) => (
+                        <Step key={label}>
+                            <StepButton onClick={handleStep(index)}>
+                                {label}
+                            </StepButton>
+                        </Step>
+                    ))
+                }
+                </Stepper>
+            </AppBar>
+        </div>
+        );
+    }
 }
 
 export default withStyles(styles)(App);
